559b9e8af9fef6e7512c0d293d8a8c80143dbf4d
mustafaintmona-arch <mustafa.intmona@gmail.com>
2026-08-15 10:20:27 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE='daily-v29';
+const ASSETS=['./','./index.html','./comps.json','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install',function(e){
+ self.skipWaiting();
+ e.waitUntil(caches.open(CACHE).then(function(c){
+  return Promise.all(ASSETS.map(function(u){
+   return c.add(u).catch(function(){});
+  }));
+ }));
+});
+self.addEventListener('activate',function(e){
+ e.waitUntil(caches.keys().then(function(ks){
+  return Promise.all(ks.map(function(k){if(k!==CACHE)return caches.delete(k);}));
+ }).then(function(){return self.clients.claim();}));
+});
+self.addEventListener('fetch',function(e){
+ var r=e.request;
+ if(r.method!=='GET')return;
+ if(new URL(r.url).origin!==self.location.origin)return;
+ e.respondWith(
+  fetch(r).then(function(res){
+   var copy=res.clone();
+   caches.open(CACHE).then(function(c){c.put(r,copy).catch(function(){});});
+   return res;
+  }).catch(function(){
+   return caches.match(r).then(function(hit){
+    return hit||caches.match('./index.html');
+   });
+  })
+ );
+});
+self.addEventListener('message',function(e){
+ if(e.data==='skipWaiting')self.skipWaiting();
+});
